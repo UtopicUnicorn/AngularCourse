876629e44583f21e11873b01c29db527d81cee28
UtopicUnicorn <medvedevtimofei@gmail.com>
2021-12-11 13:43:20 +0700

    removed unused code

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
-//import { DISHES } from '../shared/dishes';
 import { of, Observable } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -15,36 +14,17 @@ export class DishService {
   constructor(private http: HttpClient,
     private processHTTPMsgService:ProcessHTTPMsgService) { }
   getDishes(): Observable<Dish[]>{
-    //return Promise.resolve(DISHES);
-    //return new Promise(resolve=>{
-      //simulate server latency
-    //  setTimeout(()=>resolve(DISHES),2000);
-    //});
-    //return of(DISHES).pipe(delay(1));
     return this.http.get<Dish[]>(baseURL + 'dishes').pipe(catchError(this.processHTTPMsgService.handleError));
   }
   getDish(id: string): Observable<Dish>{
-    //return Promise.resolve(DISHES.filter((dish)=>(dish.id === id))[0]);
-    //return new Promise(resolve=>{
-      //simulate server latency
-     // setTimeout(()=>resolve(DISHES.filter((dish)=>(dish.id === id))[0]),2000);
-    //});
-    //return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(1));
     return this.http.get<Dish>(baseURL + 'dishes/' + id).pipe(catchError(this.processHTTPMsgService.handleError));
   }
   getFeaturedDish(): Observable<Dish>{
-    //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
-    //return  new Promise(resolve=> {
-      // Simulate server latency with 2 second delay
-     //   setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
-    //});
-    //return of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(1));
     return this.http.get<Dish[]>(baseURL + 'dishes?featured=true').pipe(map(dishes => dishes[0]))
       .pipe(catchError(this.processHTTPMsgService.handleError));
     
   }
   getDishIds(): Observable<string[] | any> {
-    //return of(DISHES.map(dish => dish.id ));
     return this.getDishes().pipe(map(dishes => dishes.map(dish => dish.id)))
     .pipe(catchError(error => error));
   }
